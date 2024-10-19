c09ba28de375d05f7f6d314db2295a60d821502f
Renata Albuquerque <44981921+renataalbq@users.noreply.github.com>
2024-10-19 16:42:54 -0300

    learn lifecycle and mvvm

---
+++ swift-projects/Flights/Flights/FlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightsApp.swift
+//  Flights
+//
+//  Created by Renata Albuquerque on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
